ec111f22e0b4bddd1b92eb2c138dd8136b76a099
ViruSilvesteR <onesilonecho@gmail.com>
2025-12-26 18:46:02 +0200

    Initial .NET MAUI project

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiCalculator")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiCalculator.Pages")]
