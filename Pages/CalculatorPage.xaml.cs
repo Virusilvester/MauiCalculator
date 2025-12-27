434a821f4136a6c18c3f301c8acfa60018cfc146
ViruSilvesteR <onesilonecho@gmail.com>
2025-12-27 16:18:27 +0200

    added Fade-In Animation

--- Pages/CalculatorPage.xaml.cs
+++ Pages/CalculatorPage.xaml.cs
@@ -11,25 +11,35 @@ public partial class CalculatorPage : ContentPage
     }
 
     private void OnHistorySelected(object sender, SelectionChangedEventArgs e)
-{
-    var selected = e.CurrentSelection.FirstOrDefault() as string;
-
-    if (selected == null)
-        return;
-
-    var parts = selected.Split('=');
-
-    if (parts.Length == 2)
     {
-        var result = parts[1].Trim();
+        var selected = e.CurrentSelection.FirstOrDefault() as string;
 
-        if (BindingContext is MauiCalculator.ViewModels.CalculatorViewModel vm)
+        if (selected == null)
+            return;
+
+        var parts = selected.Split('=');
+
+        if (parts.Length == 2)
         {
-            vm.Display = result;
+            var result = parts[1].Trim();
+
+            if (BindingContext is MauiCalculator.ViewModels.CalculatorViewModel vm)
+            {
+                vm.Display = result;
+            }
+        }
+
+    ((CollectionView)sender).SelectedItem = null;
+    }
+
+    private async void OnHistoryItemLoaded(object sender, EventArgs e)
+    {
+        if (sender is VisualElement element)
+        {
+            element.Opacity = 0; // safety
+            await element.FadeTo(1, 300, Easing.CubicIn);
         }
     }
 
-    ((CollectionView)sender).SelectedItem = null;
-}
 
 }
